0f040de6c83d8513d04035cac3e79d107daaa4fd
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-05-15 11:10:18 +0200

    Mise a jour dossier

---
+++ ECF/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ECF, creation d'un site web pour vendre des objets">
+    <link rel="stylesheet" href="./../../../css/style.css">
+    <title>Leboncote</title>
+</head>
+
+<body>
+
+
+
+
+</body>
+
+</html>
