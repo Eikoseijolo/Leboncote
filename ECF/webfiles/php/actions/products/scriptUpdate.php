809f0d2dfc337dedf1455354deb85fc168824e95
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-05-16 11:48:34 +0200

    Mise a jour general

--- ECF/webfiles/php/actions/products/scriptUpdate.php
+++ ECF/webfiles/php/actions/products/scriptUpdate.php
@@ -9,20 +9,20 @@ $database = new PDO(
 if (!empty($database)) {
     // Alors on execute la suite:
     // Creation des variables en recuperant les infos transmises avec la methode POST:
-
+    $id = $_POST["id"];
     $nom = $_POST["article"];
     $img = $_POST["img"];
     $prix = $_POST["prix"];
     $desc = $_POST["descriptions"];
     // creation variable pour la requete de mise a jour l'enssemble des tuples:
-    $req = "UPDATE vente SET article = '$nom', img = '$img', prix = '$prix', descriptions = '$desc'  WHERE id_article";
+    $req = "UPDATE vente SET article = '$nom', img = '$img', prix = '$prix', descriptions = '$desc'  WHERE id_article = '$id'";
     // creation de la variable pour executer:
     $exec = $database->query($req);
     // Si l'execution est different de false alors ca a bien été executé:
     if ($exec != false) {
         echo "ok";
         // Retour a l'index.php :
-        header("Location: ./../../../index.php");
+        header("Location: ./../../../../index.php");
         // Sinon message d'erreur :
     } else {
         echo "erreur";
