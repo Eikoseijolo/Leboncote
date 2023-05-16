0adb58ccef880281ca827aceddde1f41f51af8d4
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-05-16 09:22:27 +0200

    ajout fonction modifier

--- ECF/index.php
+++ ECF/index.php
@@ -11,18 +11,74 @@
 </head>
 
 <body>
+    <?php
+    $database = new PDO(
+        'mysql:host=localhost;dbname=leboncote;charset=utf8',
+        'root',
+        ''
+    );
+    // FAIRE UN REQUIRE ONCE DU DBCONNECT 
+    if (!empty($database)) {
+        ?>
+        <header>
+            <nav>
+                <p>Leboncote</p>
+                <ul>
+                    <li><a href="./webfiles/php/views/products/ajout.php">Ajouter</a></li>
+                    <li><a href="./webfiles/php/views/products/formUpdate.php">Modifier</a></li>
+                    <li><a href="#">Suprimer</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1>Nos ventes</h1>
 
-    <header>
-        <nav>
-            <p class="logo">Leboncote</p>
-            <ul>
-                <li><a href="./webfiles/php/views/products/ajout.php">Ajouter</a></li>
-                <li><a href="./webfiles/php/views/products/formUpdate.php">Modifier</a></li>
-                <li><a href="#">Suprimer</a></li>
-            </ul>
-        </nav>
-    </header>
+            <form action="./webfiles/php/views/products/Ajout.php" method="POST">
+                <button type="submit">Ajouter un produit</button>
+            </form>
+            <?php
+    }
+    ?>
+        <div class="container">
+            <?php
+            $exec = $database->query("SELECT * FROM vente");
+            if ($exec != false):
 
+                $res = $exec->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($res as $tuple):
+                    ?>
+                    <div class="card">
+
+                        <form action="./webfiles/php/actions/products/scriptDelete.php" method="POST">
+                            <input type="hidden" name="id" value="<?php echo $tuple["id_article"]; ?>">
+                            <button type="submit">Suprimer l'article</button>
+                        </form>
+                        <form action="./webfiles/php/views/products/formUpdate.php" method="POST">
+                            <input type="hidden" name="id" value="<?php echo $tuple["id_article"]; ?>">
+                            <div><button type="submit">Modifier l'article</button></div>
+                        </form>
+
+
+                        <img src="<?= $tuple["img"]; ?>" alt="illustration de <?= $tuple["descriptions"]; ?>">
+
+                        <input type="hidden" name="id" value="<?php echo $tuple["prix"]; ?>">
+                        <div><button class="info" type="submit">
+                                <?php echo $tuple["prix"]; ?>€&nbsp;-&nbsp; Acheter
+                            </button></div>
+
+                        <p class="product">
+                            <?= $tuple["descriptions"]; ?>
+                        </p>
+                    </div>
+                <?php endforeach; ?>
+
+            <?php else: ?>
+                <p>Requete SQL non valide.</p>
+            <?php endif; ?>
+
+        </div>
+    </main>
 </body>
 
 </html>
--- ECF/webfiles/php/actions/products/scriptAjout.php
+++ ECF/webfiles/php/actions/products/scriptAjout.php
@@ -1,23 +1,26 @@
-<?php 
-   
- // FAIRE UN REQUIRE ONCE DU DBCONNECT 
-    if(!empty($conn)){
+<?php
+$database = new PDO(
+    'mysql:host=localhost;dbname=leboncote;charset=utf8',
+    'root',
+    ''
+);
+// FAIRE UN REQUIRE ONCE DU DBCONNECT 
+if (!empty($database)) {
 
-        $nom = $_POST["descriptions"];
-        $image = $_POST["img"];
-        $prix = $_POST["prix"];
-        $article = $_POST["article"];
+    $nom = $_POST["descriptions"];
+    $image = $_POST["img"];
+    $prix = $_POST["prix"];
+    $article = $_POST["article"];
 
-        $req = "INSERT INTO vente (descriptions, img, prix, article) VALUES ('$nom', '$image', '$prix', '$article')";
+    $req = "INSERT INTO vente (descriptions, img, prix, article) VALUES ('$nom', '$image', '$prix', '$article')";
 
-        $exec = $conn->query($req);
+    $exec = $database->query($req);
 
-        if($exec != false){
-            echo "l'ajout de l'article a fonctionné";
-        }
-        else{
-            echo "l'ajout de votre article n'a pas fonctionné";
-        }
+    if ($exec != false) {
+        echo "l'ajout de l'article a fonctionné";
+    } else {
+        echo "l'ajout de votre article n'a pas fonctionné";
     }
-    
+}
+
 ?>
