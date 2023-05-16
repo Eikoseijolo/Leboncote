fcf548bb26954de3645b1c95cb21d5ffac295a22
WarrenMandrine <warren.jeuffrain@gmail.com>
2023-05-16 10:04:58 +0200

    ajout du script/ajout/dbconnect

---
+++ ECF/webfiles/php/actions/products/scriptAjout.php
@@ -0,0 +1,25 @@
+<?php 
+ 
+   require_once('./webfiles/php/actions/dbconnect.php');
+
+ // FAIRE UN REQUIRE ONCE DU DBCONNECT 
+    if(!empty($database)){
+
+        $nom = $_POST["descriptions"];
+        $image = $_POST["img"];
+        $prix = $_POST["prix"];
+        $article = $_POST["article"];
+
+        $req = "INSERT INTO vente (descriptions, img, prix, article) VALUES ('$nom', '$image', '$prix', '$article')";
+
+        $exec = $database->query($req);
+
+        if($exec != false){
+            echo "l'ajout de l'article a fonctionné";
+        }
+        else{
+            echo "l'ajout de votre article n'a pas fonctionné";
+        }
+    }
+    
+?>
