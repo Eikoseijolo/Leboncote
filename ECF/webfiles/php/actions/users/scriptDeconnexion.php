f30dfd6b58ee93a94648be9a32340aadbed43eac
unknown <warren.jeuffrain@gmail.com>
2023-05-19 11:38:15 +0200

    correction

--- ECF/webfiles/php/actions/users/scriptDeconnexion.php
+++ ECF/webfiles/php/actions/users/scriptDeconnexion.php
@@ -1,10 +1,10 @@
 <?php
-//on importe la page dbConnect.php qui sert a se connecter a la base de donnée
-require_once('./../dbConnect.php');
-
-//On desactive la session de connection, l'utilisateur n'est plus connecté.
+//On désactive la session de connexion, l'utilisateur n'est plus connecté.
 session_destroy();
-//On envoi l'utilisateur sur la page index.php
+//on importe la page dbConnect.php qui sert à se connecter a la base de données
+require_once('./../dbconnect.php');
+
+//On envoie l'utilisateur sur la page index.php
 header("Location: ./../../../../index.php");
 
 ?>
