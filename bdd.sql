90c14ff5db6a33f0d928c3c8fe6f95fe2b0d9095
WillGRD <w.gerardin@outlook.fr>
2023-05-19 13:13:03 +0200

    Export de la base de donnée

--- bdd.sql
+++ bdd.sql
@@ -19,51 +19,38 @@
 CREATE DATABASE IF NOT EXISTS `leboncote` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `leboncote`;
 
--- Listage de la structure de table leboncote. admin
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` int NOT NULL,
-  `admin_nom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `admin_prenom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `admin_password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`admin_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Listage des données de la table leboncote.admin : ~2 rows (environ)
-DELETE FROM `admin`;
-INSERT INTO `admin` (`admin_id`, `admin_nom`, `admin_prenom`, `admin_password`) VALUES
-	(1, 'warren', 'warren', '1234'),
-	(2, 'william', 'william', '1234'),
-	(3, 'vincent', 'vincent', '1234');
-
 -- Listage de la structure de table leboncote. user
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int NOT NULL,
-  `user_nom` varchar(50) DEFAULT NULL,
-  `user_prenom` varchar(50) DEFAULT NULL,
-  `user_password` varchar(50) DEFAULT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_nom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `user_password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table leboncote.user : ~0 rows (environ)
 DELETE FROM `user`;
+INSERT INTO `user` (`user_id`, `user_nom`, `user_password`) VALUES
+	(1, 'willeggzeg', 'gzegzeg'),
+	(2, 'willgergge', 'ergrrger'),
+	(3, 'will02', 'will02400');
 
 -- Listage de la structure de table leboncote. vente
 CREATE TABLE IF NOT EXISTS `vente` (
   `id_article` int NOT NULL AUTO_INCREMENT,
-  `article` varchar(50) DEFAULT NULL,
-  `descriptions` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `article` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `descriptions` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `prix` float DEFAULT NULL,
   `img` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_article`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table leboncote.vente : ~8 rows (environ)
+-- Listage des données de la table leboncote.vente : ~4 rows (environ)
 DELETE FROM `vente`;
 INSERT INTO `vente` (`id_article`, `article`, `descriptions`, `prix`, `img`) VALUES
-	(2, 'pomme', 'le sport cest bien', 200, 'https://m.media-amazon.com/images/I/61p22DCb8ZL._AC_SY879_.jpg'),
-	(3, 'pomme', 'le sport cest bien', 200, 'https://m.media-amazon.com/images/I/61p22DCb8ZL._AC_SY879_.jpg'),
-	(4, 'Velo', 'Le velo cest bien', 600, 'https://m.media-amazon.com/images/I/81tBupHDgQL._AC_SX679_.jpg'),
-	(7, 'pomme', 'ryrj,ud', 22, 'https://m.media-amazon.com/images/I/61p22DCb8ZL._AC_SY879_.jpg');
+	(9, 'Corsair', 'bar de ram', 56, 'https://m.media-amazon.com/images/I/518T3hrUY4L._AC_SL1200_.jpg'),
+	(10, 'Ecran pc', 'Trés bon ecran', 150, 'https://m.media-amazon.com/images/I/71r8p6wpZEL._AC_SL1500_.jpg'),
+	(11, 'Clavier razer', 'Présentant une finition mate propre, le boîtier est suffisamment robuste pour résister à de longues heures d’utilisation intense et régulière', 200, 'https://m.media-amazon.com/images/I/712e7S94B+L._AC_SL1500_.jpg'),
+	(12, 'Logitech Souris de jeu sans fil G703 LIGHTSPEED', 'La souris gaming Logitech G703 LIGHTSPEED rencontre la nouvelle génération du capteur HERO 25K. Devenez pro avec une précision de jeu à 200 - 25’600 dpi et zéro lissage, et des performances sans fil améliorées.', 90.55, 'https://m.media-amazon.com/images/I/5138fK1qKuL._AC_SL1028_.jpg');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
